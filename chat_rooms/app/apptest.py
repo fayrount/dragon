6d660071a3115f6bfad60e9351b8bbf936964747
fayrount <48170273@qq.com>
2018-01-06 15:27:32 +0800

    protocol apptest

--- chat_rooms/app/apptest.py
+++ chat_rooms/app/apptest.py
@@ -4,20 +4,20 @@ from firefly.server.globalobject import netserviceHandle
 from firefly.server.globalobject import GlobalObject
 import time
 import protocol.protocol_pb2
+import protocol.protocolbuf
 from twisted.python import log
 @netserviceHandle
 def echo_1(_conn,data):
-    log.msg("enter echo_1");
-    chat_msg = protocol.protocol_pb2.ChatMsg();
-    chat_msg.ParseFromString(data);
-    ret_msg = '%d said %s'%(chat_msg.id,chat_msg.content);
-    chat_msg.content = ret_msg;
-    log.msg("quit echo_1 ",chat_msg.id,chat_msg.content);
+    log.msg("enter echo_1 ",len(data));
+    pb = protocol.protocolbuf.protocolbuf();
+    chat_msg = pb.c2s_buf2data("chat_msg",data);
+    send_buf = pb.s2c_data2buf("chat_msg",chat_msg);
+    log.msg("quit echo_1 ",chat_msg.id,chat_msg.content,len(send_buf));
 
     lis = GlobalObject().netfactory.connmanager._connections.keys()     #获取所有在线用户
     lis.remove(_conn.transport.sessionno)                               #移除发言者本身，                      (补充说明：避免和客户端显示重复)
     #GlobalObject().netfactory.pushObject(1, chat_msg.SerializeToString(), [_conn.transport.sessionno])         #向用户群发送当前发言者发言信息
-    GlobalObject().netfactory.pushObject(1, "hello hello hello获取所有", [_conn.transport.sessionno])         #向用户群发送当前发言者发言信息
+    GlobalObject().netfactory.pushObject(1, send_buf, [_conn.transport.sessionno])         #向用户群发送当前发言者发言信息
     return;
     #return chat_msg.SerializeToString();
 
