39d1404f94044e6f93b12be5b8b5548c5b23c5bc
fayrount <48170273@qq.com>
2018-01-14 10:50:17 +0800

    silversmith modify bug

--- silversmith/app/protocol/protocol_def.py
+++ silversmith/app/protocol/protocol_def.py
@@ -5,7 +5,7 @@ c2s_createrole = 2;
 c2s_choserole = 3;
 c2s_move = 4;
 c2s_getrolelist = 5;
-//////////////////////////////////
+#########################################
 s2c_common_rsp = 1;
 s2c_rolelist = 2;
 s2c_enterscene = 3;
