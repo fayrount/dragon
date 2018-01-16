34b6ae2156d8f6bb6afed357ed601993e973df80
fayrount <48170273@qq.com>
2018-01-16 17:19:57 +0800

    defaultencoding

--- silversmith/app/netserver.py
+++ silversmith/app/netserver.py
@@ -5,6 +5,7 @@ Created on 2013-8-13
 @author: lan
 '''
 import sys
+reload(sys)
 sys.setdefaultencoding('utf-8') #set default encoding to utf-8
 
 from net import initconfig
--- silversmith/startmaster.py
+++ silversmith/startmaster.py
@@ -1,5 +1,6 @@
 #coding:utf8
 import sys
+reload(sys)
 sys.setdefaultencoding('utf-8') #set default encoding to utf-8
 
 import os
