5ec93b6902b341a04dd6976ecc8d3c39d8dd9692
fayrount <48170273@qq.com>
2018-10-22 17:04:53 +0800

    test net

--- fishsvr/app/game/gatenodeapp/test.py
+++ fishsvr/app/game/gatenodeapp/test.py
@@ -5,7 +5,7 @@ Created on 2013-8-14
 @author: lan
 '''
 import app.protocol.netutil as netutil
-import app.protocol.Protocol_desc as protocol_def
+import app.protocol.ProtocolDesc as protocol_def
 from twisted.python import log
 from firefly.server.globalobject import GlobalObject
 
