4724a082f8b8a71a4cf28a6409da6cc5538257c1
fayrount <48170273@qq.com>
2018-10-22 17:01:45 +0800

    test net

--- fishsvr/app/game/gatenodeapp/loginout.py
+++ fishsvr/app/game/gatenodeapp/loginout.py
@@ -7,7 +7,6 @@ Created on 2013-8-14
 from app.game.gatenodeservice import remoteserviceHandle
 from app.game.core.PlayersManager import PlayersManager
 import app.protocol.netutil as netutil
-import app.protocol.protocol_def as protocol_def
 from twisted.python import log
 from firefly.server.globalobject import GlobalObject
 
@@ -15,14 +14,5 @@ from firefly.server.globalobject import GlobalObject
 def NetConnLost_600002(dynamicId):
     '''loginout
     '''
-    player = PlayersManager().getPlayerBydynamicId(dynamicId)
-    if not player:
-        return True
-    player.updatePlayerDBInfo()
-    PlayersManager().dropPlayer(player)
-
-
-    buf = netutil.s2c_data2buf("s2c_role_out",{'id':player.getBaseID()})
-    GlobalObject().remote['gate'].callRemote("pushObjectOthers",protocol_def.s2c_role_out,buf, [dynamicId])
     return True
     
--- fishsvr/app/game/gatenodeapp/otherenter.py
+++ fishsvr/app/game/gatenodeapp/otherenter.py
@@ -8,30 +8,12 @@ from app.game.gatenodeservice import remoteserviceHandle
 from app.game.core.character.PlayerCharacter import PlayerCharacter
 from app.game.core.PlayersManager import PlayersManager
 import app.protocol.netutil as netutil
-import app.protocol.protocol_def as protocol_def
 from twisted.python import log
 from firefly.server.globalobject import GlobalObject
 
 @remoteserviceHandle
 def otherenterPlace_600003(dynamicId, characterId,data):
-    log.msg('otherenterPlace_600003 %s %s %s' % (dynamicId,characterId,data));
-
-    allothers = PlayersManager().getAll();
-    log.msg("role list ",len(allothers));
-    for i in allothers:
-        player = i;
-        id = player.getBaseID();
-        log.msg("other roles id ",id,characterId);
-        if id != characterId:
-            shape = player.shape;
-            lv = player.lv;
-            cls = player.cls;
-            name = player.name;
-            x = player.x;
-            y = player.y;
-            log.msg("other roles ",id,shape,lv,cls,name,x,y);
-            buf = netutil.s2c_data2buf("s2c_role_enter",{'id':id,'shape':shape,'class':cls,'lv':lv,'name':name,'x':x,'y':y});
-            GlobalObject().remote['gate'].callRemote("pushObject",protocol_def.s2c_role_enter,buf, [dynamicId])
+    
     return
 
                     
